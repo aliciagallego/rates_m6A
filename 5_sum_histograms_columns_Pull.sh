923b413ba7844b7d88193fb7ff1ed027d9ef0c87
aliciagallego <99732403+aliciagallego@users.noreply.github.com>
2022-08-02 20:06:27 +0200

    Update and rename 14.2_sum_histograms_columns_Pull.sh to 5_sum_histograms_columns_Pull.sh

---
+++ 5_sum_histograms_columns_Pull.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script computes total reads per histogram window across all genes
+
+for NUM in 1 2 3 4
+
+do
+	FRAC=0.01	
+	INPUT="path/Histograms/"$NUM"_Histogram/MG9-"$NUM"_matrix_"$FRAC".txt"
+	OUT="/path/Histograms/"$NUM"_Histogram"
+	OUT2="/path/Histograms"
+
+	cat $INPUT | awk '{if(NR==1){for(i=2;i<=NF;i++){h[i]=$i} next;} for(i=2;i<=NF;i++){a[i]+=$i}}END{for(i=2;i<=NF;i++){print h[i]"\t"a[i]}}'> $OUT/"$NUM"_matrix_sum_"$FRAC".txt"
+	cat $INPUT | awk '{if(NR==1){for(i=2;i<=NF;i++){h[i]=$i} next;} for(i=2;i<=NF;i++){a[i]+=$i}}END{for(i=2;i<=NF;i++){print h[i]"\t"a[i]}}'>> $OUT2/"sum_all_matrix_sum_"$FRAC".txt"
+done
+wait
