e630c2d3879c3735f4a76214be9c87035d942f03
aliciagallego <99732403+aliciagallego@users.noreply.github.com>
2022-08-02 20:12:59 +0200

    Update 15.1_Metaplot.R

--- 15.1_Metaplot.R
+++ 15.1_Metaplot.R
@@ -4,25 +4,27 @@
 ## Metaplots 4sU-DRB-seq #
 ##########################
 
+# This script generates meta-gene profiles from histograms
+# Histogram information from 0 min and 5 min (for both WT and H1-TKO samples) were previously pasted together in the same file
+
 # -------
 # Paths |
 # -------
-WT_pull_path <- "/media/cc/B/Josemi/TTseq_Feb2022/TTseq_output/Visualize_alignments/MG10_RNA_DRB-4sU/5.2_Histograms_Pull/ghist/MG9-WT_Pull_matrix_sum_001.txt"
-TKO_pull_path <- "/media/cc/B/Josemi/TTseq_Feb2022/TTseq_output/Visualize_alignments/MG10_RNA_DRB-4sU/5.2_Histograms_Pull/ghist/MG9-TKO_Pull_matrix_sum_001.txt"
-
-output <- ("/media/cc/B/Josemi/TTseq_Feb2022/TTseq_output/Visualize_alignments/MG10_RNA_DRB-4sU/5.2_Histograms_Pull/ghist/")
+WT_path <- "/path/Histograms/WT_Pull_matrix_sum_001.txt"
+TKO_path <- "/path/Histograms/TKO_Pull_matrix_sum_001.txt.txt"
+output <- "path/Histograms/"
 
 # -----------
 # Open data |
 # -----------
 WT_pull <- read.table(WT_pull_path,h=T,sep="\t",stringsAsFactors=FALSE)
 TKO_pull <- read.table(TKO_pull_path,h=T,sep="\t",stringsAsFactors=FALSE)
-head(WT_pull)
-tail(TKO_pull)
 
-# ----------
-# Metaplot |
-# ----------
+# -----------------
+# Meta-gene plots |
+# -----------------
+
+# H1-TKO profiles
 png(file = paste0(output, "Metaplot_TTseq_TKO_Pull.png"))
 plot(TKO_pull$X, TKO_pull$TKO5_mean,type="l", ylim=c(0,60000),xlim=c(-2000,50000),lwd=2, col="red",
      xlab = "Gene body (-2Kb to +50Kb)",
@@ -32,6 +34,7 @@ legend("topright", c("TKO 0'","TKO 5'"),
        col=c("grey","red"), lwd=2, inset =.02, cex=0.9)
 dev.off()
 
+# WT profiles
 png(file = paste0(output, "Metaplot_TTseq_WT_Pull.png"))
 plot(WT_pull$X, WT_pull$WT5_mean,type="l", ylim=c(0,60000),xlim=c(-2000,50000),lwd=2, col="blue",
      xlab = "Gene body (-2Kb to +50Kb)",
