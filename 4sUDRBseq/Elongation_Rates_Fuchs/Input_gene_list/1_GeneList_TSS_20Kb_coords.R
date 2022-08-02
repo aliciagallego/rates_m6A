cf6c5be0dd19380c9bb0ddf955951cdb1de24abf
aliciagallego <99732403+aliciagallego@users.noreply.github.com>
2022-08-02 20:32:12 +0200

    Update and rename 4sUDRBseq/1.1_loop_TSS_20Kb_coords.R to 4sUDRBseq/Elongation_Rates_Fuchs/Input_gene_list/1_GeneList_TSS_20Kb_coords.R

--- 4sUDRBseq/Elongation_Rates_Fuchs/Input_gene_list/1_GeneList_TSS_20Kb_coords.R
+++ 4sUDRBseq/Elongation_Rates_Fuchs/Input_gene_list/1_GeneList_TSS_20Kb_coords.R
@@ -1,21 +1,24 @@
 #!/usr/bin/env Rscript
 
-############################################
-## Retain the first 20 Kb from TSS (start) # 20220325
-############################################
+####################################
+## Retain the first 20 Kb from TSS # 
+####################################
+
+# This script tranforms gene coordinates to keep TSS and TSS+20kb
+# This gene list is generated to just retain those genes that show transcription from the TSS
 
 # -------
 # Paths |
 # -------
-refseq_path <- "/media/cc/A/Alicia/Genome_files/Josemi/RefSeq_genes.bed"
-output <- "/media/cc/B/Josemi/TTseq_Feb2022/TTseq_data/"
+refseq_path <- "/path/RefSeq_genes.bed"
+output <- "/path/output"
 
 # -----------
 # Open data |
 # -----------
 refseq <- read.table(refseq_path,h=F,sep="\t",stringsAsFactors=FALSE,
                      col.names = c("Chr","Start","End","Gene_name","NA1","Strand"))
-head(refseq)
+
 # ----------------------------------------
 # Transform start - 2Kb and start + 50Kb |
 # ----------------------------------------
@@ -48,20 +51,18 @@ print(count)
 
 # Drop columns 7 and 8
 refseq <- refseq[-c(7,8)]
-head(refseq)
 
-# --------------------------------------------------------------------------------------
-# Remove transcripts with negative coords (these are mitochondrial and rare sequences) |
-# --------------------------------------------------------------------------------------
-aa<- refseq$V1=="chrMT" #0
-aa
-refseq <- refseq[!refseq$Start < 0,] #2 genes
-nrow(refseq)
+# -----------------------------------------
+# Remove transcripts with negative coords |
+# -----------------------------------------
+# This step removes mitochondrial and rare sequences
+refseq <- refseq[!refseq$Start < 0,] 
+
 # -----------
 # Save data |
 # -----------
 write.table(refseq, 
-            file = paste0(output,"RefSeq_LongList_TSS_20Kb.bed"),
+            file = paste0(output,"RefSeq_TSS_20Kb.bed"),
             quote = F, 
             sep="\t", 
             col.names = F, 
