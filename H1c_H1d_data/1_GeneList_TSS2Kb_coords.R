8941161d86d6200f1d8816f9eaa1b279f5db116f
aliciagallego <99732403+aliciagallego@users.noreply.github.com>
2022-08-03 18:01:51 +0200

    Create 1_GeneList_TSS2Kb_coords.R

---
+++ H1c_H1d_data/1_GeneList_TSS2Kb_coords.R
@@ -0,0 +1,58 @@
+#!/usr/bin/env Rscript
+
+#####################################################################
+## Define promoter coordinates (TSS ±2Kb) from RefSeq transcriptome #
+#####################################################################
+
+# This script uses a transcriptome BED list as input and defines the promoter coordinates as TSS ±2Kb
+
+# -------
+# Paths |
+# -------
+refseq_path <- "/path/Genome_files/RefSeq_genes.bed"
+output <- "/path/H1_Cao/Intersect/"
+
+# -----------
+# Open data |
+# -----------
+refseq <- read.table(refseq_path,h=F,sep="\t",stringsAsFactors=F)
+
+# --------------------------------------------------
+# Generate new start/end coords from previous ones |
+# --------------------------------------------------
+count <- 0
+for(i in 1:nrow(refseq)) {
+  if(refseq[i,6] == '+') {
+    refseq[i,7]=(refseq[i,2]-2000)
+    refseq[i,8]=(refseq[i,2]+2000)
+  }
+  else if(refseq[i,6] == '-'){
+    refseq[i,7]=(refseq[i,3]-2000)
+    refseq[i,8]=(refseq[i,3]+2000)
+  }
+  else {
+    refseq[i,7]=NA
+    refseq[i,8]=NA
+  }
+  count = count+1
+}
+print(count)
+
+# Replace col 2 by col 8 and col 3 by col 9
+count <- 0
+for(i in 1:nrow(refseq)) {
+  refseq[i,2]=refseq[i,7]
+  refseq[i,3]=refseq[i,8]
+  count = count+1
+}
+print(count)
+
+# Drop columns 8 and 9
+refseq <- refseq[-c(7,8)]
+
+# -----------
+# Save data |
+# -----------
+write.table(refseq, 
+            file = paste0(output,"Input_genes_RefSeq_Long_List_2KbTSS.bed"),
+            quote = F, sep="\t", col.names = F, row.names = F) 
