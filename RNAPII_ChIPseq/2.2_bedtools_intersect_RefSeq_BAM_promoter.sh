e59f99644eaa03db49006e9a42db51af61ed5b9b
aliciagallego <99732403+aliciagallego@users.noreply.github.com>
2022-08-04 18:53:14 +0200

    Create 2.2_bedtools_intersect_RefSeq_BAM_promoter.sh

---
+++ RNAPII_ChIPseq/2.2_bedtools_intersect_RefSeq_BAM_promoter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script intersects (bedtools function) the aligned BAM sequences and the BED transcriptome list that include the promoter coordinates
+
+AFILE=/path/RNAPII/Gene_selection_RefSeq/Input_genes_RefSeq_Long_List_Promoters.bed
+BAM=/path/RNAPII/Alignment
+
+for FILE in $BAM/*.bam
+do
+	NAME=$(basename $FILE | sed 's/_R.*//')
+  bedtools intersect -c -wa -a $AFILE -b $FILE  > /path/RNAPII/Intersect_bedtools/Promoters/$NAME'_wa_PR_500bp'.bed
+
+done
