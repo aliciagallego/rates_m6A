716d2f4cda35ecfcb5f1e96545a99a1f3ef13e66
aliciagallego <99732403+aliciagallego@users.noreply.github.com>
2022-08-04 22:00:44 +0200

    Update and rename 5.2_filter_histograms_outliers_pull_geeven_tertiles.sh to Histone_marks_data_Geeven/4_filter_histograms_outliers.sh

--- Histone_marks_data_Geeven/4_filter_histograms_outliers.sh
+++ Histone_marks_data_Geeven/4_filter_histograms_outliers.sh
@@ -1,43 +1,46 @@
 #!/bin/bash
 
+# This script removes the 0.1% most expressed genes from the previously generated histograms
+# The % of genes that will be removed ($FRAC) should be adjusted according to the data
+
 for HIST in H3K4me1 H3K4me3 H3K9me3 H3K27me3 
 do 
 	for REP in WT TKO
 	do
-
 		for GROUP in slow med fast
 		do
-			INPUT="/media/cc/B/Alicia/Geeven/Geeven_output/3.2_Histograms_tertiles/"$HIST"/"$REP"_"$GROUP"/"$HIST"_"$REP"_"$GROUP"output.txt" 
-			OUT="/media/cc/B/Alicia/Geeven/Geeven_output/3.2_Histograms_tertiles/"$HIST"/"$REP"_"$GROUP
-			FRAC=0.01	
+			INPUT="/path/Histograms/"$HIST"/"$REP"_"$GROUP"/"$HIST"_"$REP"_"$GROUP"output.txt" 
+			OUT="/path/Histograms/"$HIST"/"$REP"_"$GROUP
 
-			# obtener el numero total de filas (genes) sin contar la fila del encabezado
+			# FRAC value ihas to be defined after prior analyses 
+	                # In this case the 0.1% of the most expressed genes will we removed
+			FRAC=0.01
+			
+			# get total number of rows (genes) excluding the header 
 			MYNUM=$(cat $INPUT | sed '1d' | wc -l)
 
-			# multiplicar el numero total de filas por $FRAC y sumar 1
-			# bc -l indica usar formulas matemáticas
-			# sed quita los decimales 4.55 -> 4
+			# multiply total row number by 0.05 and sum +1
+			# sed remove decimals e.g. 4.55 -> 4
 			MYNUM=$(echo "($MYNUM * $FRAC) + 1" | bc -l | sed 's/\..*//')
 
 			# Threshold
 			# sed "1d" Prints the contents of file excluding the first line to the standard output
-			# sumar todas las columnas por fila
-			# sed 's/\..*//':  quita los decimales
-			# sort -nr: ordena de mayor a menor
-			# head -n $MYNUM: selecciona los $MYNUM primeros
-			# tail -1: selecciona el último de los valores $MYNUM (el número a partir del cual se van a eliminar los valores altos o threshold)
-
+			# sum all columns by row
+			# sed 's/\..*//':  removes decimals
+			# sort -nr: sorts from higher to lower values
+			# head -n $MYNUM: selects the first $MYNUM values
+			# tail -1: selects the last value from the $MYNUM selectin (it is the value from which the higher values will be removed or threshold)
 			THRES=$(cat $INPUT | sed '1d' | awk -v OFS="\t" '{for(i=2;i<=NF;i++) sum+=$i; print sum; sum=0}' | sed 's/\..*//' | sort -nr  | head -n $MYNUM | tail -1)
 
 			printf "up on the threshold\t"$MYNUM"\n" > $OUT/$HIST"_"$REP"_"$GROUP"_summary_"$FRAC".txt"
 			printf "threshold\t"$THRES"\n" >> $OUT/$HIST"_"$REP"_"$GROUP"_summary_"$FRAC".txt"
 
-			# imprimir los encabezados
+			# prints headers
 			cat $INPUT | head -1 > $OUT/$HIST"_"$REP"_"$GROUP"_matrix_"$FRAC".txt"
 
 			# sed "1d": Prints the contents of file excluding the first line to the standard output
-			# sumar todas las columnas por fila
-			# seleccionar aquellas filas cuya columna sumatoria es menor al threhold anteriormente calculado
+			# sum all columns by row
+			# selects all rows which summatory column is lower than the previously computed threshold
 			cat $INPUT | sed '1d' | awk -v OFS="\t" '{for(i=2;i<=NF;i++) sum+=$i; print $0,sum; sum=0}' | awk -v OFS="\t" -v thres=$THRES '($NF<thres){printf $1"\t"; for (i=2; i<NF; i++) printf $i"\t"; printf "\n"}' >> $OUT/$HIST"_"$REP"_"$GROUP"_matrix_"$FRAC".txt"     	 	
 
 done
