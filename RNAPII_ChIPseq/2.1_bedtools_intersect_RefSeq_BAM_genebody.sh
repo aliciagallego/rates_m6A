920a1864ab8bf52d5813fcdd683bc7b16d9f9c31
aliciagallego <99732403+aliciagallego@users.noreply.github.com>
2022-08-04 18:51:36 +0200

    Create 2.1_bedtools_intersect_RefSeq_BAM_genebody.sh

---
+++ RNAPII_ChIPseq/2.1_bedtools_intersect_RefSeq_BAM_genebody.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script intersects (bedtools function) the aligned BAM sequences and the BED transcriptome list that include the gene body coordinates
+
+AFILE=/path/RNAPII/Gene_selection_RefSeq/Input_genes_RefSeq_Long_List_GeneBody.bed
+BAM=/path/RNAPII/Alignment
+
+for FILE in $BAM/*.bam
+do
+
+	NAME=$(basename $FILE | sed 's/_S.*//') 
+       	bedtools intersect -c -wa -a $AFILE -b $FILE > /path/RNAPII/Intersect_bedtools/GeneBody/$NAME'_wa_GB_500bp'.bed
+
+done
