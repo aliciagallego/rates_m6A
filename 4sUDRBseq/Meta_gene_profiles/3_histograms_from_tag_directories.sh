592bbd8bb143497ace9b566a6342fbde231ee9f0
aliciagallego <99732403+aliciagallego@users.noreply.github.com>
2022-08-04 23:18:14 +0200

    Update 3_histograms_from_tag_directories.sh

--- 4sUDRBseq/Meta_gene_profiles/3_histograms_from_tag_directories.sh
+++ 4sUDRBseq/Meta_gene_profiles/3_histograms_from_tag_directories.sh
@@ -4,8 +4,15 @@
 # The transcriptome contains gene coordinates TSS-2kb and TSS+50kb
 # annotatePeaks.pl is a function from HOMER
 
-TAG="/path/TagDirectory/"
-TRANSCRIPTOME="/path/RefSeq_coding_TSS_2Kb50Kb.bed
-OUT="/path/Histogram"      
+TRANSCRIPTOME="/path/RefSeq_coding_TSS_2Kb50Kb.bed"
 
-annotatePeaks.pl $TRANSCRIPTOME none -size 52000 -hist 20 -ghist -d $TAG > $OUT/output.txt"     	 	
+for NUM in WT0 WT5 WT15 WT45 TKO0 TKO5 TKO15 TKO45
+do
+
+	TAG="/path/"$NUM"_TagDirectory/"
+	OUT="/path/Histograms/"$NUM"_Histogram"
+	annotatePeaks.pl $TRANSCRIPTOME none -size 52000 -hist 20 -ghist -d $TAG > $OUT/"$NUM"_output.txt"     	 	
+
+
+done
+wait
