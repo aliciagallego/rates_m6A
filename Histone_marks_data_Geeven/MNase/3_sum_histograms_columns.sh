9d04b68c3a3f283c786f051c9eb5708334833f69
aliciagallego <99732403+aliciagallego@users.noreply.github.com>
2022-08-04 22:35:00 +0200

    Update and rename Histone_marks_data_Geeven/3_sum_histograms_columns.sh to Histone_marks_data_Geeven/MNase/3_sum_histograms_columns.sh

--- Histone_marks_data_Geeven/MNase/3_sum_histograms_columns.sh
+++ Histone_marks_data_Geeven/MNase/3_sum_histograms_columns.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script computes total reads per histogram window across all genes
+
 for CEL in WT TKO
 do
 	for REP in I II
@@ -8,8 +10,8 @@ do
 		do
 
 			FRAC=0.01	
-			INPUT="/media/cc/C/Alicia/MNase/histograms/"$CEL"_"$REP"/"$CEL"_"$REP"_"$GROUP"_matrix_"$FRAC".txt"
-			OUT="/media/cc/C/Alicia/MNase/histograms/"$CEL"_"$REP
+			INPUT="/path/MNase/Hstogram/"$CEL"_"$REP"/"$CEL"_"$REP"_"$GROUP"_matrix_"$FRAC".txt"
+			OUT="/path/MNase/Hstogram/"$CEL"_"$REP
 
 			cat $INPUT | awk '{if(NR==1){for(i=2;i<=NF;i++){h[i]=$i} next;} for(i=2;i<=NF;i++){a[i]+=$i}}END{for(i=2;i<=NF;i++){print h[i]"\t"a[i]}}'> $OUT/$CEL"_"$REP"_"$GROUP"_matrix_sum_"$FRAC".txt"
 
